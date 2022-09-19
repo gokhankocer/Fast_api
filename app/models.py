9c180ccebc2b93af5edb2b079e2c530305fc7aec
gokhankocer <gkkocer@gmail.com>
2022-09-19 13:17:16 +0300

    Updateed

---
+++ app/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from .db import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, nullable=False)
+
+
+class Items(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
